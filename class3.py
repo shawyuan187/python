4a935b53a92af7cae2480249ec2db0d56a9ad0ff
shawyuan <mewtwox360@gmail.com>
2023-10-15 11:01:37 +0800

    class3

---
+++ class3.py
@@ -0,0 +1,69 @@
+"""
+name = "python"
+age = 31
+new_str = "我是" + name + "," + "今年" + str(age) + "歲了"
+print(new_str)
+
+
+name = "python"
+age = 31
+new_str_1 = "我是%s, 今年%d歲了" % (name, age)
+print(new_str_1)
+
+try:
+    num = int(input("請輸入一格數字"))
+    total = num + 1
+    print(total)
+except:
+    print("都是電腦的錯🤣")
+
+
+我好棒
+
+==  相等
+!=  不相等
+>=  大於等於
+<=  小於等於
+>   大於
+<   小於
+
+and:
+
+false and flase false
+false and true  false
+true and false  false
+true and true   true
+
+or:
+
+false or false  false
+false or true   true
+true or false   true 
+true or true    true
+
+()括號
+**次方
++正數
+-負數
++-*/加減乘除
+%餘數
+//取商
+== ,!= ,> ,< ,>= ,<=
+
+password = input("請輸入密碼:")
+if password == "1487":
+    print("歡迎光臨")
+if password != "1487":
+    print("笑死你超智障lmao")
+
+
+else
+
+if:
+    password = input("請輸入密碼:")
+    if password == "1487":
+        print("歡迎光臨")
+else:
+    print("密碼錯誤")
+
+"""
