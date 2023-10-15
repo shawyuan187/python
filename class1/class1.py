330c930439755afb9e41a68f0abcda3b42f3f04b
shawyuan <mewtwox360@gmail.com>
2023-10-15 09:50:05 +0800

    class3

---
+++ class1/class1.py
@@ -0,0 +1,17 @@
+print("lol")
+"""
++
+-
+*
+/
+//
+%
+**
+"""
+print(1 + 2)
+print(1 - 1)
+print(5 * 5)
+print(10 / 2)
+print(10 // 4)
+print(25 % 4)
+print(35**2)
---
+++ class2/class2.py
@@ -0,0 +1,27 @@
+# e=max(1,2,3)取最大值
+
+# print(type(True))資料物件
+# print(type(123))
+# print(type(123.0))
+# #print(type("hello"))
+
+# print(int(True))
+# print(int("123"))
+# print(int("hello"))#將輸入值轉換成整數型態
+
+# print(float(True)) 將輸入值轉換為浮點數
+
+# print(bool(0))
+# print(bool(1))
+# print(bool(-2))
+# print(bool("hello"))將輸入值轉換成布林型態
+
+# print(str(True))
+# print(str(1e44))
+
+# a = input("type your word")#提示文字
+# print(a)#一律都是字串 顯示a
+a = input("your age :")
+a = int(a) + 10  # 目前是A是整數
+
+print("你十年後得年齡:" + str(a))
